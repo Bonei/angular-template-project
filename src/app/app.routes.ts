072d9e0cebee0be938987409680fc8cbba2b34e6
Arthur César <boneiedifi@gmail.com>
2025-02-23 18:04:11 -0300

    feat: 14-Lazy loading nas rotas para otimizar o bundle

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,20 @@ import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'como-funciona', component: AboutComponent },
-    { path: 'login', component: LoginComponent }
+    {
+        path: 'como-funciona', 
+        loadComponent () {
+            return import('./pages/about/about.component').then(
+                (m) => m.AboutComponent
+            );
+        },
+    },
+    {
+        path: 'login', 
+        loadComponent () {
+            return import('./pages/login/login.component').then(
+                (m) => m.LoginComponent
+            );
+        },
+    }
 ];
