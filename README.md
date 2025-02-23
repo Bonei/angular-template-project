02db5ff221f7523798dae4ffe84482cc90e207cb
Arthur César <boneiedifi@gmail.com>
2025-02-23 17:10:24 -0300

    refactor: Name of project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# WebsiteAngular
+# Gocoffee
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 19.1.8.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'website-angular' title`, () => {
+  it(`should have the 'gocoffee' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('website-angular');
+    expect(app.title).toEqual('gocoffee');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, website-angular');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, gocoffee');
   });
 });
