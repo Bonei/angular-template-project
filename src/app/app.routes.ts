68dcd1138a886a0b9cb0e15d69585b985d2ef29e
Arthur César <boneiedifi@gmail.com>
2025-02-23 18:11:45 -0300

    feat: 15-Rota com redirect padrão ou estratégia error 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { AboutComponent } from './pages/about/about.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { PathNotFoundComponent } from './pages/path-not-found/path-not-found.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
@@ -20,5 +21,6 @@ export const routes: Routes = [
                 (m) => m.LoginComponent
             );
         },
-    }
+    },
+    {path: '**', component: PathNotFoundComponent },  
 ];
